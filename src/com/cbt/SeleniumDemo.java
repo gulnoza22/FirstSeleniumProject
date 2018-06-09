c5b3466975501bb02a30df59397bab3561d3ede4
Gulnoza <tadjichka@gmail.com>
2018-06-09 16:03:40 -0500

    add Firefox

--- src/com/cbt/SeleniumDemo.java
+++ src/com/cbt/SeleniumDemo.java
@@ -3,22 +3,24 @@ package com.cbt;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class SeleniumDemo {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "C:/Users/Gulnoza/Documents/selenium dependencies/drivers/chromedriver.exe");
-//		System.setProperty("webdriver.gecko.driver", "C:/Users/Gulnoza/Documents/selenium dependencies/drivers/geckodriver.exe");
+//		System.setProperty("webdriver.chrome.driver", "C:/Users/Gulnoza/Documents/selenium dependencies/drivers/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:/Users/Gulnoza/Documents/selenium dependencies/drivers/geckodriver.exe");
 //		System.setProperty("webdriver.edge.driver", "C:/Users/Gulnoza/Documents/selenium dependencies/drivers/MicrosoftWebDriver.exe");
 //		System.setProperty("webdriver.ie.driver", "C:/Users/Gulnoza/Documents/selenium dependencies/drivers/IEDriverServer.exe");
 		
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver1 = new ChromeDriver();
+		WebDriver driver2 = new FirefoxDriver();
 //		WebDriver driver = new EdgeDriver();
 //		WebDriver driver = new InternetExplorerDriver();
-		driver.get("http://www.cybertekschool.com");
-		driver.close();
+		driver2.get("http://www.cybertekschool.com");
+//		driver1.close();
 
 	}
 
